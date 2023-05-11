4a080673bd60cea0a7591a8a4d768a3e4d8fb5e5
chokchok2400 <92856612+chokchok2400@users.noreply.github.com>
2023-05-12 01:39:57 +0900

    Add files via upload

--- src/pages/studentstate/StudentState.js
+++ src/pages/studentstate/StudentState.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import './StudentState.css';
 import Draggable from "react-draggable";
 import axios from 'axios'; 
+import React, { useState, useEffect } from 'react';
 
 function allowDrop(event) {
   event.preventDefault();
 }
 
-function handleDrag(event, ui) {
-  // Do something when an item is dragged
+function handleDrag(event, ui, student, setStudent) {
+  // Update the student's state when an item is dragged
+  const id = event.target.id;
+  const newStudent = student.map((item) => {
+    if (item.id === id) {
+      return { ...item, state: ui.lastX > 700 ? "left" : ui.lastX < 300 ? "right" : "center" };
+    }
+    return item;
+  });
+  setStudent(newStudent);
 }
 
-function drop(event) {
+function drop(event, student, setStudent) {
   event.preventDefault();
   var data = event.dataTransfer.getData("text");
   var target = event.target;
@@ -20,12 +28,16 @@ function drop(event) {
     target = target.parentNode;
   }
 
-  var item = document.getElementById(data);
-  target.appendChild(item);
-
+  const newStudent = student.map((item) => {
+    if (item.id === data) {
+      return { ...item, state: target.id };
+    }
+    return item;
+  });
+  setStudent(newStudent);
 }
 
-function sortItems(box) {
+const sortItems = (box) => {
   var items = box.querySelectorAll('.item');
   var sortedItems = Array.prototype.slice.call(items).sort(function(a, b) {
       return a.innerHTML.localeCompare(b.innerHTML, 'ko-KR');
@@ -35,133 +47,60 @@ function sortItems(box) {
   }
 }
 
-function StudentState() {
+const StudentState = () => {
+  const [student, setStudent] = useState([]);
 
-  axios.post('/getStudentInfo').then(function(response){
-    console.log(response)
-  }).catch(function(reason){
-    console.log(reason);
-  });
+  const UserList = ({ users }) => {
+    return (
+        <div>
+            {users.map(user => {
+                return (<div key={user.id}>
+                    {user.name}
+                </div>)
+            })}
+        </div>
+    );
+};
 
   return (
     <div className="StudentState">
-      <div className="box" id="box1" onDrop={drop} onDragStart={allowDrop}>
+      <div className="box" id="box1" onDrop={(event) => drop(event, student, setStudent)} onDragStart={allowDrop}>
         <h3>도착하지 않았어요</h3>
-        <Draggable onStop={handleDrag}>
-          <div className="item" id="item1" draggable="true">김하민</div>
-        </Draggable>
-        <Draggable onStop={handleDrag}>
-          <div className="item" id="item2" draggable="true">이시원</div>
-        </Draggable>
-        <Draggable onStop={handleDrag}>
-          <div className="item" id="item3" draggable="true">박주안</div>
-        </Draggable>
+        {student.filter((item) => item.state === "box1").map(student => (
+          <Draggable onStop={(event, ui) => handleDrag(event, ui, student, setStudent)}>
+            <div className="item" id={student.id} draggable="true">{student.name}</div>
+          </Draggable>
+        ))}
       </div>
 
-      <div className="box" id="box2" onDrop={drop} onDragOver={allowDrop}>
+      <div className="box" id="box2" onDrop={(event) => drop(event, student, setStudent)} onDragOver={allowDrop}>
         <h3>돌봄교실에 도착했어요</h3>
+        {student.filter((item) => item.state === "box2").map(student => (
+          <Draggable onStop={(event, ui) => handleDrag(event, ui, student, setStudent)}>
+            <div className="item" id={student.id} draggable="true">{student.name}</div>
+          </Draggable>
+        ))}
       </div>
 
-      <div className="box" id="box3" onDrop={drop} onDragOver={allowDrop}>
+      <div className="box" id="box3" onDrop={(event) => drop(event, student, setStudent)} onDragOver={allowDrop}>
           <h3>방과후수업에 다녀올게요</h3>
-      </div>
-
-      <div className="box" id="box4" onDrop={drop} onDragOver={allowDrop}>
-          <h3>오늘은 떠날게요</h3>
-      </div>
+          {student.filter((item) => item.state === "box3").map(student => (
+          <Draggable onStop={(event, ui) => handleDrag(event, ui, student, setStudent)}>
+          <div className="item" id={student.id} draggable="true">{student.name}</div>
+        </Draggable>
+      ))}
     </div>
+
+    <div className="box" id="box4" onDrop={(event) => drop(event, student, setStudent)} onDragOver={allowDrop}>
+        <h3>오늘은 떠날게요</h3>
+        {student.filter((item) => item.state === "box4").map(student => (
+          <Draggable onStop={(event, ui) => handleDrag(event, ui, student, setStudent)}>
+            <div className="item" id={student.id} draggable="true">{student.name}</div>
+          </Draggable>
+        ))}
+    </div>
+  </div>
   );
-}
+};
 
 export default StudentState;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 재윤님이 처음 쓰신 코드
-
-import './StudentState.css';
-import Draggable from "react-draggable"; 
-import axios from 'axios';
-
-var draggedItem = null;
-
-function allowDrop(event) {
-  event.preventDefault();
-}
-
-function drag(event) {
-  draggedItem = event.target;
-  event.dataTransfer.setData("text", event.target.id);
-}
-
-function drop(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData("text");
-  var target = event.target;
-
-  while (target.className != "box") {
-    target = target.parentNode;
-  }
-
-  if (target.id != draggedItem.parentNode.id) {
-    target.appendChild(draggedItem);
-    sortItems(target);
-  }
-}
-
-function sortItems(box) {
-        var items = box.querySelectorAll('.item');
-        var sortedItems = Array.prototype.slice.call(items).sort(function(a, b) {
-            return a.innerHTML.localeCompare(b.innerHTML, 'ko-KR');
-        });
-        for (var i = 0; i < sortedItems.length; i++) {
-            box.appendChild(sortedItems[i]);
-        }
-    }
-
-
-function StudentState() {
-  
-  axios.post('/getStudentInfo').then(function(response){
-    console.log(response)
-  }).catch(function(reason){
-    console.log(reason);
-  });
-  
-    return (
-      <div className="StudentState">
-        <div className="box" id="box1" onDrop={drop} onDragStart={allowDrop}>
-          <h3>도착하지 않았어요</h3>
-          <div className="item" id="item1" draggable="true" onDragStart={drag}>김하민</div>
-          <div className="item" id="item2" draggable="true" onDragStart={drag}>이시원</div>
-          <div className="item" id="item3" draggable="true" onDragStart={drag}>박주안</div>
-        </div>
-
-        <div className="box" id="box2" onDrop={drop} onDragOver={allowDrop}>
-          <h3>돌봄교실에 도착했어요</h3>
-        </div>
-
-        <div className="box" id="box3" onDrop={drop} onDragOver={allowDrop}>
-            <h3>방과후수업에 다녀올게요</h3>
-        </div>
-
-        <div className="box" id="box4" onDrop={drop} onDragOver={allowDrop}>
-            <h3>오늘은 떠날게요</h3>
-        </div>
-    </div>
-    );
-  }
-  
-  export default StudentState;
-  */
