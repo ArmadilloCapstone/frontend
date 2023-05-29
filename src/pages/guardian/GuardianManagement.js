9f78f9e65153a684a16b1662deb136a6cd21fa30
cocosome <kcsgaram123@gmail.com>
2023-05-29 13:15:16 +0900

    학생리스트 이름 변경 students -> studentList

--- src/pages/guardian/GuardianManagement.js
+++ src/pages/guardian/GuardianManagement.js
@@ -118,7 +118,7 @@ export default function GuardianManagement() {
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
                                 <button class="add-student-button" onClick={() => {
-                                    setAddStudents(!addstudentList)
+                                    setAddstudentList(!addstudentList)
                                     onEdit(el);
                                 }}>추가
                                 </button>
