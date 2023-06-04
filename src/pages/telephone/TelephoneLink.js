6938c459656f5f6251894c8e475c8e77414008e8
Soooobiniya <rorngk22@gmail.com>
2023-06-04 19:23:06 +0900

    긴급연락 프론트 구현 완료

--- src/pages/telephone/TelephoneLink.js
+++ src/pages/telephone/TelephoneLink.js
@@ -2,60 +2,39 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './style.css';
 import teacherImg from './teacher.png';
-import schoolImg from './school-round.png';
-import buildingImg from './building.png';
 
 function TelephoneLink() {
   const [callList, setCallList] = useState([
-    { name: "돌봄교사", phone_num: "010-2532-7535" },
-    { name: "아주초등학교", phone_num: "010-2532-7535" },
-    { name: "아주초 행정실", phone_num: "010-2532-7535" }
+    {name: "수빈", phone_num: "010-2532-7535"},
+    {name: "왜", phone_num: "010-1111-1111"}
   ]);
 
   // 주요 연락처 List 가져오기
-  const loadCallList = async () => {
-    try {
-      const response = await axios.post('http://dolbomi.site/callList');
-      const formattedData = response.data.map((el) => {
-        return {
-          name: el.name,
-          phone_num: el.phone_num
-        };
-      });
-      setCallList(formattedData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
-    loadCallList();
+    axios.post('http://dolbomi.site/callList', {
+      parent_id: localStorage.getItem('userid')
+    })
+      .then(function (response) {
+        console.log(response.data);
+        setCallList(response.data.map(function (el) {
+          var returnObj = {}
+          returnObj['name'] = el.name;
+          returnObj['phone_num'] = el.phone_num;
+          return returnObj;
+        }));
+      }).catch(function (reason) {
+        console.log(reason);
+      });
   }, []);
-
-  function phoneCall(phoneNumber, name) {
-    const imageMap = {
-      "010-2532-7535": teacherImg,
-      "010-1234-5678": schoolImg,
-      "010-9876-5432": buildingImg,
-    };
-
-    const image = imageMap[phoneNumber] || teacherImg;
-
-    return (
-      <div>
-      <a href={"tel:" + phoneNumber}>
-        <img src={image} alt={name} />
-        <span>{name}</span>
-      </a>
-    </div>
-  );
-  }
 
   return (
     <div className="telephone-link-container">
       {callList.map((el, index) => (
         <div className="telephone-link-item" key={index}>
-          {phoneCall(el.phone_num, el.name)}
+            <a href={"tel:" + el.phone_num}>
+              <img src={teacherImg} alt={el.name} />
+              <span>{el.name}</span>
+            </a>
         </div>
       ))}
     </div>
