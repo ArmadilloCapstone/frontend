b8f5804a9df35e971fd0c4475c2eb92ab2d1f2e3
Soooobiniya <rorngk22@gmail.com>
2023-06-05 13:44:19 +0900

    학부모 안내장, 사진첩 추가

---
+++ src/pages/parentBbs/ParentBbsList.js
@@ -0,0 +1,216 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Pagination from "react-js-pagination";
+
+import "./bbslist.css";
+import "./bbspage.css";
+
+function ParentBbsList() {
+
+    // DB에 저장되어 있는 news 리스트 가져오기 위한 변수
+    const [bbsList, setBbsList] = useState([]);
+    const [currentbbsList, setCurrentbbsList] = useState([]);
+
+    // 검색용 Hook
+    const [choiceVal, setChoiceVal] = useState("");
+    const [searchVal, setSearchVal] = useState("");
+
+    // Paging
+    const [page, setPage] = useState(1);
+    const [totalCnt, setTotalCnt] = useState(0);
+
+    // Link 용 (함수) 
+    let navigate = useNavigate();
+
+    /* [POST /bbs]: 게시글 목록 가져오기 */
+    const loadBbsList = async (choice, search) => {
+        if (choice === "all" && search === "all") {
+            await axios.post('http://dolbomi.site/BbsList', {
+                parent_id : localStorage.getItem('userid')
+            })
+                .then((res) => {
+                    console.log("[BbsList.js] useEffect() 성공");
+                    console.log(res.data);
+
+                    setBbsList(res.data);
+                    setTotalCnt(res.data.length);
+                    console.log(res.data.length)
+                    setCurrentbbsList([]);
+                    if(res.data.length >= 10){
+                        console.log("Hi")
+                        res.data.map((el, idx) => {
+                            console.log(idx)
+                            if(idx < 10){
+                                setCurrentbbsList((prev) => ([
+                                    ...prev,
+                                    el
+                                ]));
+                            }
+                        })
+                    }
+                    else{
+                        res.data.map((el) => {
+                            setCurrentbbsList((prev) => ([
+                                ...prev,
+                                el
+                            ]));
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log("[BbsList.js] useEffect() 실패");
+                    console.log(err);
+
+                });
+        }
+
+        else {
+            console.log(searchVal, choiceVal)
+            await axios.post(`http://dolbomi.site/BbsList/search`, {
+                parent_id : localStorage.getItem('userid'),
+                keyword : searchVal,
+                option : choiceVal
+            })
+                .then((res) => {
+                    console.log("[BbsList.js] useEffect() 성공");
+                    console.log(res.data);
+
+                    setBbsList(res.data);
+                    setTotalCnt((res.data.length));
+                    console.log(res.data.length)
+                    setCurrentbbsList([]);
+                    if(res.data.length >= 10){
+                        console.log("Hi")
+                        res.data.map((el, idx) => {
+                            console.log(idx)
+                            if(idx < 10){
+                                setCurrentbbsList((prev) => ([
+                                    ...prev,
+                                    el
+                                ]));
+                            }
+                        })
+                    }
+                    else{
+                        res.data.map((el) => {
+                            setCurrentbbsList((prev) => ([
+                                ...prev,
+                                el
+                            ]));
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log("[BbsList.js] useEffect() 실패");
+                    console.log(err);
+
+                });
+        }
+    }
+
+    useEffect(() => {
+        loadBbsList("all", "all");
+    }, []);
+
+    bbsList.sort((a, b) => b.id - a.id);
+    currentbbsList.sort((a, b) => b.id - a.id); // 최신순 정렬
+
+    const changeChoice = (event) => { setChoiceVal(event.target.value); }
+    const changeSearch = (event) => { setSearchVal(event.target.value); }
+    const search = () => {
+        console.log("[BbsList.js searchBtn()] choiceVal=" + choiceVal + ", searchVal=" + searchVal);
+
+        navigate("/ParentBbslist");
+        loadBbsList(choiceVal, searchVal);
+    }
+
+    const changePage = (page) => {
+        setPage(page);
+        setCurrentbbsList([]);
+        for(let i = (page-1) * 10; i < (page) * 10; i++){
+            console.log(i)
+            if(bbsList[i] != null){
+                setCurrentbbsList((prev) => ([
+                    ...prev,
+                    bbsList[i]
+                ]));
+            }
+        }
+    }
+
+
+    return (
+
+        <section class="BbListSection" style={{ fontFamily: "Eorinai" }}>
+                        <div className="BbSearchContainer">
+                            <select class="BbListSearch-1" value={choiceVal} onChange={changeChoice}>
+                                <option>검색 옵션</option>
+                                <option value="title">제목</option>
+                                <option value="text">내용</option>
+                            </select>
+                            <input  class="BbListSearch-2" type="text" placeholder="검색어" value={searchVal} onChange={changeSearch} />
+                        <div className="BbButton">
+                            <button class="BbListSearch-3" type="button" onClick={search}>검색</button>
+                        </div>
+                        </div>
+
+            <table class="BbList-0"  >
+                <thead class="BbList-0" >
+                    <tr class="BbList" >
+                        <th >번호</th>
+                        <th >제목</th>
+                        <th >작성일시</th>
+                    </tr>
+                </thead>
+
+
+
+
+
+                <tbody style={{ fontFamily: "Eorinai" }}>
+					{
+						currentbbsList.map(function (bbs, idx) {
+							return (
+								<TableRow obj={bbs} key={idx} cnt={idx + 1} page={page}/>
+							)
+						})
+					}
+				</tbody>
+            </table>
+
+            <Pagination className="pagination"
+                activePage={page}
+                itemsCountPerPage={10}
+                totalItemsCount={totalCnt}
+                pageRangeDisplayed={5}
+                prevPageText={"‹"}
+                nextPageText={"›"}
+                onChange={changePage} />
+
+        </section>
+    );
+}
+
+/* 글 목록 테이블 행 컴포넌트 */
+function TableRow(props) {
+    const bbs = props.obj;
+
+    return (
+        <tr class="BbList">
+            <th class="BbList" style={{ textAlign: "center" }} >{props.cnt + ( (props.page-1) * 10)}</th>
+            {
+                    <>
+                        <td class="BbList" >
+                            <Link to={{ pathname: `/ParentBbsDetail/${bbs.id}` }}> 
+                                <span >{bbs.title} </span> 
+                            </Link>
+                        </td>
+                        <td class="BbList">{bbs.date}</td>
+                    </>
+            }
+        </tr>
+    );
+}
+
+export default ParentBbsList;
---
+++ src/pages/parentGallery/ParentGalleryDetail.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function ParentGalleryDetail() {
+
+    const [gallery, setGallery] = useState({});
+    const [files, setFiles] = useState([]);
+    const { id } = useParams(); // 파라미터 가져오기
+
+    const navigate = useNavigate();
+
+    const getGalleryDetail = async () => {
+        console.log("hi")
+        await axios.post(`http://dolbomi.site/album/${id}`)
+            .then((resp) => {
+                console.log("[GalleryDetail.js] getGalleryDetail() success :D");
+                console.log(resp.data);
+
+                setGallery(resp.data);
+                setFiles(resp.data.file_url);
+            })
+            .catch((err) => {
+                console.log("[GalleryDetail.js] getGalleryDetail() error :<");
+                console.log(err);
+            });
+    }
+
+    useEffect(() => {
+        getGalleryDetail();
+        console.log(id)
+    }, []);
+
+    return (
+
+        <div className="detailPage" style={{ fontFamily: "Eorinai" }}>
+            <table className="table table-striped" style={{ fontFamily: "Eorinai" }}>
+                <tbody style={{ color: "#555555" }}>
+
+                    <tr>
+                        <th>제목</th>
+                        <td>
+                            <span>{gallery.title}</span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>작성일</th>
+                        <td>
+                            <span>{gallery.uploaded_date}</span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>내용</th>
+                        <td>
+                            <div>
+                                {gallery.contents}
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th className="col-3">첨부파일</th>
+                        <td>
+                            <div>
+                                <img src={"http://dolbomi.site/download/album/" + gallery.title + "/" + gallery.file_url} />
+                                <div>{gallery.file_url}</div>
+                                <div>
+                                    <a
+                                        href={"http://dolbomi.site/download/album/" + gallery.title + "/" + gallery.file_url}
+                                        download
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        다운로드
+                                    </a>
+                                </div>
+                            </div>
+
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+            <div className="my-3 d-flex justify-content-center">
+                <Link className="btn btn-outline-secondary" to="/ParentGalleryList"><i className="fas fa-list"></i> 글목록</Link>
+            </div><br /><br />
+
+        </div>
+    );
+}
+
+export default ParentGalleryDetail;
