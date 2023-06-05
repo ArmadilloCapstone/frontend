fac5055da357e73ccffb7bde97d8113fc0c63784
jaeyoon Cheong <jyjung9910@gmail.com>
2023-06-05 13:39:28 +0900

    관리자페이지 pagination 완료

--- src/pages/admin/parent/ParentDetail.js
+++ src/pages/admin/parent/ParentDetail.js
@@ -1,15 +1,49 @@
 import '../adminPages.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Pagination from "react-js-pagination";
 
 function ParentDetail() {
   const [record, setRecord] = useState([]);
+  const [currentrecord, setCurrentrecord] = useState([]);
+
+    // Paging
+    const [page, setPage] = useState(1);
+    const [totalCnt, setTotalCnt] = useState(0);
+    const changePage = (page, copy) => {
+      if(copy != undefined){
+        setPage(page);
+        setCurrentrecord([]);
+        for(let i = (page-1) * 10; i < (page) * 10; i++){
+            console.log(i)
+            if(copy[i] != null){
+                setCurrentrecord((prev) => ([
+                    ...prev,
+                    copy[i]
+                ]));
+            }
+        }
+      }
+      else{
+        setPage(page);
+        setCurrentrecord([]);
+        for(let i = (page-1) * 10; i < (page) * 10; i++){
+            console.log(i)
+            if(record[i] != null){
+                setCurrentrecord((prev) => ([
+                    ...prev,
+                    record[i]
+                ]));
+            }
+        }
+      }
+  }
 
   // On Page load display all records 
   const loadParentDetail = async () => {
     await axios.post('http://dolbomi.site/parent')
       .then(function (response) {
-        setRecord(response.data.map(function (el, idx) {
+        let item_list = response.data.map(function (el, idx) {
           console.log(el);
 
           var returnObj = {}
@@ -21,7 +55,32 @@ function ParentDetail() {
           returnObj['child_name'] = el.child_name;
 
           return returnObj;
-        }));
+        })
+        console.log(item_list)
+        setRecord(item_list);
+        setTotalCnt(response.data.length);
+        console.log(response.data.length)
+        setCurrentrecord([]);
+        if(response.data.length >= 10){
+            console.log("Hi")
+            item_list.map((el, idx) => {
+                console.log(idx)
+                if(idx < 10){
+                  setCurrentrecord((prev) => ([
+                        ...prev,
+                        el
+                    ]));
+                }
+            })
+        }
+        else{
+          item_list.map((el) => {
+            setCurrentrecord((prev) => ([
+                    ...prev,
+                    el
+                ]));
+            })
+        };
       }).catch(function (reason) {
         console.log(reason);
       });
@@ -47,11 +106,13 @@ function ParentDetail() {
     let copy = [...record];
     copy.sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
   const sortByStudentName = () => {
     let copy = [...record];
     copy.sort((a, b) => a.child_name.toUpperCase() < b.child_name.toUpperCase() ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
 
   return (
@@ -78,7 +139,7 @@ function ParentDetail() {
           </thead>
           <tbody>
 
-            {record.map((name) =>
+            {currentrecord.map((name) =>
               <tr class="admin">
                 <td class="admin">{name.name}</td>
                 <td class="admin">{name.phone_num}</td>
@@ -100,6 +161,14 @@ function ParentDetail() {
             )}
           </tbody>
         </table>
+        <Pagination className="pagination"
+                activePage={page}
+                itemsCountPerPage={10}
+                totalItemsCount={totalCnt}
+                pageRangeDisplayed={5}
+                prevPageText={"‹"}
+                nextPageText={"›"}
+                onChange={changePage} />
       </section>
     </div>
   )
--- src/pages/admin/student/StudentDetail.js
+++ src/pages/admin/student/StudentDetail.js
@@ -1,15 +1,48 @@
 import '../adminPages.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Pagination from "react-js-pagination";
 
 function StudentDetail() {
   const [record, setRecord] = useState([]);
+  const [currentrecord, setCurrentrecord] = useState([]);
 
+    // Paging
+    const [page, setPage] = useState(1);
+    const [totalCnt, setTotalCnt] = useState(0);
+    const changePage = (page, copy) => {
+      if(copy != undefined){
+        setPage(page);
+        setCurrentrecord([]);
+        for(let i = (page-1) * 10; i < (page) * 10; i++){
+            console.log(i)
+            if(copy[i] != null){
+                setCurrentrecord((prev) => ([
+                    ...prev,
+                    copy[i]
+                ]));
+            }
+        }
+      }
+      else{
+        setPage(page);
+        setCurrentrecord([]);
+        for(let i = (page-1) * 10; i < (page) * 10; i++){
+            console.log(i)
+            if(record[i] != null){
+                setCurrentrecord((prev) => ([
+                    ...prev,
+                    record[i]
+                ]));
+            }
+        }
+      }
+  }
   // On Page load display all records 
   const loadStudentDetail = async () => {
     await axios.post('http://dolbomi.site/student') // student와 parent를 연결해서 어떻게 데이터를 가져오는 거지? db 쿼리로 처리하는 건가?
       .then(function (response) {
-        setRecord(response.data.map(function (el, idx) {
+        let item_list = response.data.map(function (el, idx) {
           console.log(el);
 
           var returnObj = {}
@@ -23,7 +56,32 @@ function StudentDetail() {
           returnObj['birth_date'] = el.birth_date;
 
           return returnObj;
-        }));
+        })
+        console.log(item_list)
+        setRecord(item_list);
+        setTotalCnt(response.data.length);
+        console.log(response.data.length)
+        setCurrentrecord([]);
+        if(response.data.length >= 10){
+            console.log("Hi")
+            item_list.map((el, idx) => {
+                console.log(idx)
+                if(idx < 10){
+                  setCurrentrecord((prev) => ([
+                        ...prev,
+                        el
+                    ]));
+                }
+            })
+        }
+        else{
+          item_list.map((el) => {
+            setCurrentrecord((prev) => ([
+                    ...prev,
+                    el
+                ]));
+            })
+        }
       }).catch(function (reason) {
         console.log(reason);
       });
@@ -49,21 +107,25 @@ function StudentDetail() {
     let copy = [...record];
     copy.sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
   const sortByGrade = () => {
     let copy = [...record];
     copy.sort((a, b) => a.grade < b.grade ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
   const sortByDolbom = () => {
     let copy = [...record];
     copy.sort((a, b) => a.class_name.toUpperCase() < b.class_name.toUpperCase() ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
   const sortByOriginal = () => {
     let copy = [...record];
     copy.sort((a, b) => a.original_class_num < b.original_class_num ? -1 : 1);
     setRecord(copy);
+    changePage(page, copy)
   }
 
   return (
@@ -98,7 +160,7 @@ function StudentDetail() {
           </thead>
           <tbody>
 
-            {record.map((name) =>
+            {currentrecord.map((name) =>
               <tr class="admin">
                 <td class="admin">{name.name}</td>
                 <td class="admin">{name.grade}</td>
@@ -123,6 +185,14 @@ function StudentDetail() {
             )}
           </tbody>
         </table>
+        <Pagination className="pagination"
+                activePage={page}
+                itemsCountPerPage={10}
+                totalItemsCount={totalCnt}
+                pageRangeDisplayed={5}
+                prevPageText={"‹"}
+                nextPageText={"›"}
+                onChange={changePage} />
 
       </section >
     </div>
